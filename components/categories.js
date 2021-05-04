2ffdaaa7449155538dcb534ae77b84f572774bd7
Carlos Eberhardt <carlos@stepzen.com>
2021-05-04 09:39:37 -0500

    poor hack to fix categories

--- components/categories.js
+++ components/categories.js
@@ -1,16 +1,19 @@
 export default function Categories({ categories }) {
-  return (
-    <span className="ml-1">
-      under
-      {categories.length > 0 ? (
-        categories.map((category, index) => (
-          <span key={index} className="ml-1">
-            {category.category.name}
-          </span>
-        ))
-      ) : (
-        <span className="ml-1">{categories.category.name}</span>
-      )}
-    </span>
-  )
+  if (categories.length > 0) {
+    return (
+      <span className="ml-1">
+        under
+        {categories.length > 0 ? (
+          categories.map((category, index) => (
+            <span key={index} className="ml-1">
+              {category.category.name}
+            </span>
+          ))
+        ) : (
+          <span className="ml-1"></span>
+        )}
+      </span>
+    );
+  }
+  return <span className="ml-1"></span>;
 }
